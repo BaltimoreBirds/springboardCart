621d3e8616c42d32ea24d8e84530e93543ebfb00
Michael Swanson <mikeswanson12@msn.com>
2016-02-20 15:19:32 -0500

    defines the local storage getters and setters

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# springboardCart  
-Shopping Cart plugin for Springboard  
+# Springboard Shopping Cart  
+Shopping Cart module for Springboard
+
+Add this module definition to all pages utilizing the shopping cart. Use the SbCart API methods in your regular page wrapper scripts to Initialize, Add, Remove, Checkout, etc. with the SbCart module:   
 
 Initialize Cart: SbCart.shop({options}); Dev's option to use the default cart.
 ```
 	SbCart.shop({
 		usingDefaultCart: true,
-		exposed: true,
 		bgColor: 'teal', 
 	});
 ```
@@ -21,3 +22,8 @@ Remove Products: SbCart.remove(productName, quantity [,callback]);
 		//do something with data
 	});
 ```	
+
+
+The key to this thing comes from unique ID's attached to the different products. A 'Member-Plus' Membership, selling at $80.75 for instance, could have an ID of `AIC_Member_Plus_80.75`. This will be sold as a 'Ticket' through a Springboard 'Ticketed Event' E-commerce form. In this case, the 'Checkout' form(Form ID 1451). All possible tickets sold by this organization(AIC for example), should be available through this form. It is up to the springboardCart module users to add new options to this checkout form, via Ticketed Event >> Checkout[form] >> Tickets >> Tickets >> Add Ticket Type. 
+
+The Module, on `SbCart.checkout()`, will scan these available 'tickets' on the checkout form, and select it with the proper quantity. The user shouldn't see the available tickets at all, and the module users can display the items in cart as they see fit, as the product object gets returned to them(Or simply keep the defaultCart dropdown open)
--- springboardCart.js
+++ springboardCart.js
@@ -14,25 +14,26 @@
 	//Private Vars
 	var products = {},
 			// validProducts = [], 
-			currentPrice = 0, 
+			currentPrice = 0,
 			cartWrap,
 			error = {
 				message: " ",
 			},
 			defaults = {
-				shopping: false,
+				sessionName: null,
 				usingDefaultCart: false,
 				minHeight: 30,
 				bgColor: '#9d1b31',
 				txtColor: 'white',
 				secondaryColor: '#d8d8d8',
+				brandLogo: 'https://artic.gospringboard.com/files/artic/swan_crop2.png',
 				cartImgUrl: 'https://artic.gospringboard.com/files/artic/empty-cart-light-small.png',
-			};	
+			};
 
 	//Private functions
 	var _updateCartView = function(){
 		
-		//Clear cart innerWrap html 		
+		//Clear cart innerWrap html
 		$('#productDisplay .innerWrap')[0].innerHTML = " ";
 
 		//iterate through products, and display them
@@ -41,12 +42,12 @@
 					price = products[key].price,
 					quant = products[key].quantity;
 
-			$('#productDisplay .innerWrap').append('<div class="cartRow borderBottom"><div class="span5">'+productName+'</div><div class="span2">Qty. '+quant+'</div><div class="span3 txtRight">$'+parseFloat(quant*price).toFixed(2)+'</div></div>');
+			$('#productDisplay .innerWrap').append('<div class="cartRow borderBottom"><div class="noStretch span5">'+productName+'</div><div class="noStretch span2">Qty. '+quant+'</div><div class="noStretch span3 txtRight">$'+parseFloat(quant*price).toFixed(2)+'</div></div>');
 		});
 
 		//Build a 'total' row
 		var total = _total();
-		$('#productDisplay .innerWrap').append('<div class="cartRow"><div class="span7">Total: </div><div class="span3 txtRight">$'+total+'</div></div>');
+		$('#productDisplay .innerWrap').append('<div class="cartRow"><div class="noStretch span7">Total: </div><div class="noStretch span3 txtRight">$'+total+'</div></div>');
 	};
 
 	//return total price
@@ -56,7 +57,7 @@
 			total += (products[key].quantity * products[key].price);
 		});
 		return parseFloat(total).toFixed(2);
-	};		
+	};
 
 	//return products in cart
 	var _products = function(){
@@ -67,18 +68,19 @@
 			}
 		});
 		return productArray;
-	}
+	};
 
 	//show and hide our Default cart
 	var _cartToggle = function(){
 		$('#productDisplay').slideToggle();
-	}
+	};
 
 	//Output help
 	var _debug = function(){
-		console.log("Defaults", defaults);
-	}
+		console.log("localStorage", localStorage.getItem(defaults.sessionName));
+	};
 
+	//tests if func argument is a function
 	var _isFunction = function(func){
 		//return callback if callback is kosher
 		if(typeof func === "function"){
@@ -86,7 +88,23 @@
 		}else{
 			return false;
 		}
-	}
+	};
+
+	var _setSessionDefaults = function(){
+		localStorage.setItem(defaults.sessionName.toString(), JSON.stringify(defaults));
+	};
+
+	var _getSessionDefaults = function(options){
+		defaults = JSON.parse(localStorage.getItem(options.sessionName)) || defaults;
+	};
+
+	var _setSessionProducts = function(){
+		localStorage.setItem(defaults.sessionName+'Products', JSON.stringify(products));
+	};
+
+	var _getSessionProducts = function(){
+		products = JSON.parse(localStorage.getItem(defaults.sessionName+'Products')) || products;
+	};
 
 	//Public API 
 	return {
@@ -94,28 +112,30 @@
 		//Initialize with or without the default frame (#springboardCartWrap)
 		shop: function(options){
 
-			//Only run method if it hasn't been initialized
-			if(defaults.shopping){
-				return;
-			}
-			defaults.shopping = true;
+			//get defaults from session if they are there
+			_getSessionDefaults(options);
 
 			//merge user options with the defaults.
-			var userOptions = $.extend(true, {}, defaults, options);
-			defaults.usingDefaultCart = userOptions.usingDefaultCart;
+			$.extend(defaults, options);
 
-			if(userOptions.usingDefaultCart){
+			//pull products from localStorage object
+			_getSessionProducts();
+			//Set defaults on localStorage object 'sessionName'
+			_setSessionDefaults();
+
+			//Check user wants default cart, and that its not already there
+			if(defaults.usingDefaultCart && !$('#springboardCartWrap').length ){
 
 				//Affix shopping Cart bar. HTML in cart.html
-				$('body').prepend('<div id="springboardCartWrap" class="affixed"><div class="bar"><div class="cartRow"><div id="logoBlock" class="span2"><p>BRAND</p></div><a class="clickable" href="#"><div id="cartDropdown" class="span2 txtRight"><p><img class="iconWidth" src="'+defaults.cartImgUrl+'"/> <span>Shopping Cart <span class="arrowIcon">&#9660;</span></span></p></div></a> </div></div><div id="productDisplay" class="cartRow"><div class="innerWrap span4"></div></div></div>');
+				$('body').prepend('<div id="springboardCartWrap" class="affixed"><div class="bar borderBottom"><div class="cartRow"><div id="logoBlock" class="noStretch span5"><p><img  src="'+defaults.brandLogo+'"/> Swan & Son</p></div><a class="clickable" href="#"><div id="cartDropdown" class="noStretch span5 txtRight"><p><img class="iconWidth" src="'+defaults.cartImgUrl+'"/> <span>Shopping Cart <span class="arrowIcon">&#9660;</span></span></p></div></a> </div></div><div id="productDisplay" class="cartRow"><div class="innerWrap span4"></div></div></div>');
 
 				//Style it
-				$('#springboardCartWrap #productDisplay').hide();		
-				$('#springboardCartWrap .bar, #springboardCartWrap .clickable').css('color', userOptions.txtColor);
-				$('#springboardCartWrap .bar').css('background-color', userOptions.bgColor);
-				$('#springboardCartWrap .bar').css('min-height', userOptions.minHeight);
-				$('#springboardCartWrap .innerWrap').css('background-color',userOptions.secondaryColor);
-				$('#springboardCartWrap .innerWrap').css('border','15px solid '+userOptions.bgColor);
+				//$('#springboardCartWrap #productDisplay').hide();
+				$('#springboardCartWrap .bar, #springboardCartWrap .clickable').css('color', defaults.txtColor);
+				$('#springboardCartWrap .bar').css('background-color', defaults.bgColor);
+				$('#springboardCartWrap .bar').css('min-height', defaults.minHeight);
+				$('#springboardCartWrap .innerWrap').css('background-color',defaults.secondaryColor);
+				$('#springboardCartWrap .innerWrap').css('border','15px solid '+defaults.bgColor);
 
 				//Create handler for cart down click
 				$('#cartDropdown').on("click", function(){
@@ -124,17 +144,16 @@
 
 				//Call view updater function
 				_updateCartView();
-
 			}
 		},
 
 		//Add a product to the Cart
 		add: function(productName, quant, productPrice, callback){
 
-			//Check it's a valid product name, return error if it isn't
-			//if( validProducts.indexOf(productName) < 0 ){
-				//return error
-			//}
+			//Validate the function arguments
+			if(typeof productName !== 'string' || typeof quant !== 'number' || typeof productPrice !== 'number'){
+				throw new Error('Improper function arguments -> SbCart.add(\'string\',\'number\',\'number\')');
+			}
 
 			//If product is in cart
 			if(productName in products){
@@ -153,15 +172,21 @@
 				_updateCartView();
 			}
 
+			//Set products onto the session
+			_setSessionProducts();
+
 			//return callback if callback is kosher
 			if( _isFunction(callback) ){
 				callback(products);
 			}
+
 		},
 
 		remove: function(productName, quant, callback){
-			if(typeof quant !== "number"){
-				throw new Error("Enter a quantity ie) SbCart.remove(productName, quantity)");
+			
+			//Validate function arguments
+			if(typeof productName !== "string" || typeof quant !== "number"){
+				throw new Error("Improper Function Arguments -> SbCart.remove(productName, quantity)");
 			}
 
 			if(productName in products){
@@ -169,10 +194,16 @@
 				if(products[productName].quantity - quant <= 0 ){
 					delete products[productName];
 				}else{
-				  products[productName].quantity -= quant;
+					products[productName].quantity -= quant;
 				}
-				_updateCartView();
-			}			
+				//Update the default view if we're using it
+				if(defaults.usingDefaultCart){
+					_updateCartView();
+				}
+
+				//Set products onto the session
+				_setSessionProducts();
+			}
 
 			//return callback if callback is kosher
 			if( _isFunction(callback) ){
